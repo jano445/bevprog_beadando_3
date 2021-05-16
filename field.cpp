2f2236cc69d2d4a7919eab85f2eb2a9b78c1b7ce
jano445 <68127562+jano445@users.noreply.github.com>
2021-05-16 14:01:03 +0200

    Field class created

---
+++ field.cpp
@@ -0,0 +1,58 @@
+#include "field.h"
+
+using namespace std;
+using namespace genv;
+
+field::field(int x, int y, int sx, int sy)
+:widget(x,y,sx,sy)
+{
+    player=0;
+}
+
+void field::draw() const
+{
+    switch(player)
+    {
+    case 0:
+        gout<<move_to(x_,y_)
+            <<color(255,255,255)
+            <<box(size_x, size_y);
+        gout<<move_to(x_+1,y_+1)
+            <<color(0,0,0)
+            <<box(size_x-2, size_y-2);
+        break;
+
+    case 1:
+        gout<<move_to(x_,y_)
+            <<color(255,255,255)
+            <<box(size_x, size_y);
+        gout<<move_to(x_+1,y_+1)
+            <<color(255,0,0)
+            <<box(size_x-2, size_y-2);
+        break;
+
+    case 2:
+        gout<<move_to(x_,y_)
+            <<color(255,255,255)
+            <<box(size_x, size_y);
+        gout<<move_to(x_+1,y_+1)
+            <<color(0,0,255)
+            <<box(size_x-2, size_y-2);
+        break;
+    }
+}
+
+void field::handle(event ev)
+{
+    extern unsigned int turn; //átvenni ezt a változót
+    if(ev.type == ev_mouse && is_selected(ev.pos_x, ev.pos_y) && ev.button == btn_left && player == 0)
+    {
+        player=turn%2==0 ? 1:2;
+        turn++;
+    }
+}
+
+void field::resetfield()
+{
+    player=0;
+}
---
+++ field.h
@@ -0,0 +1,18 @@
+#ifndef FIELD_H
+#define FIELD_H
+
+#include <widget.h>
+
+
+class field : public widget
+{
+    int player;
+    public:
+        field(int x, int y, int sx, int sy);
+        virtual void draw() const;
+        virtual void handle(genv::event ev);
+        virtual unsigned int getplayer();
+        virtual void resetfield();
+};
+
+#endif // FIELD_H
